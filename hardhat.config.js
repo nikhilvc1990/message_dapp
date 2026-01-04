305ed92f67be04189aa1be45648ad9f5185d1861
nikhilvc1990 <nikhil.chandran@iiitmk.ac.in>
2026-01-04 21:46:47 +0530

    changing to sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.28",
-  defaultNetwork: "localhost",
+  defaultNetwork: "sepolia",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
@@ -15,9 +15,9 @@ module.exports = {
         accountsBalance: "900000000000000000000000",
       },
     },
-    // sepolia : {
-    //   url: "YOUR_SEPOLIA_RPC_URL",
-    //   accounts: ["YOUR_PRIVATE_KEY"]
-    // }
+     sepolia : {
+       url: "https://sepolia.infura.io/v3/276dac1864714950b920c06b3fc7a2a7",
+       accounts: ["213f8a4dfbcf2ee8dd2cb42a72ca071ce137f2c9d713f82677c95ddb5178ba16"]
+     }
   }
 };
